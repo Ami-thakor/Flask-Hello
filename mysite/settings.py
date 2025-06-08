d313b2934efb8b3c18c2451cbdc206d92295ca11
Ami-thakor <128568711+Ami-thakor@users.noreply.github.com>
2025-06-08 21:03:14 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-59e7+avdsz_jv6z^mdlo@q$q0_+quz(f5^(6_-8-d6agrf+uez'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
